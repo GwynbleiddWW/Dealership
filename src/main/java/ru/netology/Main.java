e8040cb61ab86107a5fb471a49cb7bcabfbc3c28
Konstantin Shebalin <championacolironni@gmail.com>
2021-05-20 01:03:41 +0700

    Commit...

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -4,22 +4,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         final CarFactory carFactory = new CarFactory();
 
-
         ThreadGroup buyers = new ThreadGroup("group1");
         new Thread(buyers, carFactory::sellAuto, "Первый покупатель").start();
         new Thread(buyers, carFactory::sellAuto, "Второй покупатель").start();
         new Thread(buyers, carFactory::sellAuto, "Третий покупатель").start();
         new Thread(buyers, carFactory::sellAuto, "Четвёртый покупатель").start();
-
+        new Thread(buyers, carFactory::sellAuto, "Пятый покупатель").start();
 
         ThreadGroup makers = new ThreadGroup("group2");
         new Thread(makers, carFactory::createAuto, "Производитель Toyota").start();
         new Thread(makers, carFactory::createAuto, "Производитель Subaru").start();
         new Thread(makers, carFactory::createAuto, "Производитель Nissan").start();
         new Thread(makers, carFactory::createAuto, "Производитель BMW").start();
-
-        /*Если создать равное количество потоков покупателей-создателей и убрать цикл, то программа худо-бедно работает.
-        Но это опять же абсолютно не то.
-        Вопрос: как сделать так, чтобы авто выпускались постоянно и при этом только только при пустом магазине?*/
+        new Thread(makers, carFactory::createAuto, "Производитель Audi").start();
     }
 }
--- src/main/java/ru/netology/Seller.java
+++ src/main/java/ru/netology/Seller.java
@@ -1,8 +1,7 @@
 package ru.netology;
 
 public class Seller {
-
-    private final int sleepTimeout = 1500;
+    private final int timeout = 2000;
     private final CarFactory carFactory;
 
     public Seller(CarFactory carFactory) {
@@ -10,47 +9,26 @@ public class Seller {
     }
 
     public synchronized Auto sellAutos() {
-           /*Человек заходит в магазин, продавец ему говорит, что машин нет.
-            Человек - это поток. Машинет нет в салоне, и пока список  auto пуст, мы выдаём сообщение, что их нет.
-            Поток останавливаем до уведомления от другого потока, что машина создана и добавлена в список auto.
-             */
-        try {
-            Thread.sleep(sleepTimeout);
-            System.out.println(Thread.currentThread().getName() + " зашел в автосалон");
-            while (carFactory.getAuto().size() == 0) {
-                Thread.sleep(sleepTimeout);
-                System.out.println("Машин нет в наличии");
+        System.out.println(Thread.currentThread().getName() + " зашел в автосалон");
+        if (carFactory.getAuto().isEmpty()) {
+            System.out.println("Машин нет в наличии");
+            try {
                 wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            Thread.sleep(sleepTimeout);
-            /*Всё, авто добавлено в список, он не пуст - можно покупать-уезжать.
-            Если использовать if, то машин никогда не будет потом.
-            Если while, то бесконечный список.*/
-            if (carFactory.getAuto().size() != 0) {
-                Thread.sleep(sleepTimeout);
-                System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
-                /*wait(); Если здесь ожидаем, то не отрабатывает удаление из списка, если не будет wait(),
-                 то будет бесконечный список.*/
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-        return carFactory.getAuto().remove(0); /*Здесь мы снова создаём ситуацию отсутствия авто, чтобы производитель снова начал выпускать авто.*/
+        System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
+        return carFactory.getAuto().remove(0);
     }
 
     public synchronized void makeAuto() {
-        /*Здесь происходит добавлнение в список новой машины, если список пуст.*/
-        try {
-            //while (carFactory.getAuto().size() == 0) {
-            /*Здесь и без цикла получается добавлять в список. Если его использовать то какое условие?*/
-                Thread.sleep(sleepTimeout);
-                carFactory.getAuto().add(new Auto(" \"Prototype X\" "));
-                System.out.println(Thread.currentThread().getName() + " выпустил 1 авто:" + carFactory.getAuto().listIterator().next().name);
-                notify();
-            //} Выходит что у меня не получается сдеать так, чтобы один поток создания обрабатывал все запросы потоков-покупателей.
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        Auto auto = new Auto(" \"Prototype X\" ");
+        int amountOfAuto = 10;
+        if (carFactory.getAuto().size() < amountOfAuto) {
+            carFactory.getAuto().add(auto);
+            notify();
         }
+        System.out.println(Thread.currentThread().getName() + " выпустил 1 авто:" + auto.name);
     }
 }
